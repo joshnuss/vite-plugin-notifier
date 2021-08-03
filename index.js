2a87e7c973e951e31f42b1e3de9a50fd1c3abd51
Josh Nussbaum <joshnuss@gmail.com>
2021-08-03 04:33:41 -0400

    Only applies to serve

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ module.exports = function notifierPlugin() {
   const virtualFileId = '/vite-plugin-notifier'
   const absolutePath = path.join(__dirname, './notifier.js')
   const source = fs.readFileSync(absolutePath, 'utf-8')
-  let config
 
   return {
     name: 'vite-plugin-notifier',
+    apply: 'serve',
     resolveId(id) {
       if (id == virtualFileId) {
         return id
@@ -20,10 +20,6 @@ module.exports = function notifierPlugin() {
       }
     },
     transformIndexHtml(html) {
-      if (config.command !== 'serve') {
-        return
-      }
-
       return [
         {
           tag: 'script',
@@ -31,11 +27,6 @@ module.exports = function notifierPlugin() {
         }
       ]
     },
-
-    configResolved(resolvedConfig) {
-      config = resolvedConfig
-    },
-
     handleHotUpdate({server}) {
       server.ws.send({
         type: 'custom',
